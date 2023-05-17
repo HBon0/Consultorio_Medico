b00610f36ce2fc85ea4c46c36004465b8374ef23
Hector Bonilla <bonilla.osorio0@gmail.com>
2023-05-17 16:06:18 -0600

    Agregando Logger a PatientController

--- Consultorio_Medico.WVC/Controllers/PatientController.cs
+++ Consultorio_Medico.WVC/Controllers/PatientController.cs
@@ -17,22 +17,27 @@ namespace Consultorio_Medico.MVC.Controllers
     public class PatientController : Controller
     {
         readonly IPatientBL _patientBL;
-        public PatientController(IPatientBL patientBL)
+        private readonly ILogger<PatientController> _logger;
+        public PatientController(IPatientBL patientBL, ILogger<PatientController> logger)
         {
-          _patientBL = patientBL;
+            _patientBL = patientBL;
+            _logger = logger;
         }
         // GET: PatientController
         public async Task<ActionResult> Index(patientSearchInputDTO patient)
         {
+            _logger.LogInformation("-------------- INICIO METODO INDEX PATIENT CONTROLLER ----------------------");
             var list = await _patientBL.Search(patient);
+            _logger.LogInformation("--------------- FIN METODO INDEX PATIENT CONTROLLER ------------------");
             return View(list);
         }
 
         // GET: PatientController/Details/5
         public async Task<ActionResult> Details(int Id)
         {
+            _logger.LogInformation("------------- INICIO METODO DETAILS PATIENT CONTROLLER -------------------");
             var Patient = await _patientBL.GetById(Id);
-
+            _logger.LogInformation("-------------- FIN METODO DETAILS PATIENT CONTROLLER ---------------------");
             return View(Patient);
         }
 
@@ -50,12 +55,17 @@ namespace Consultorio_Medico.MVC.Controllers
         {
             try
             {
+                _logger.LogInformation("----------------- INICIO METODO CREATE POST PATIENT CONTROLLER ---------------");
                 int result = await _patientBL.Create(patient);
                 if (result > 0)
-
+                {
+                    _logger.LogInformation("------------ REGISTRO CREADO CORRECTAMENTE EN CREATE POST PATIENT CONTROLLER ------------");
                     return RedirectToAction(nameof(Index));
+                }
+                    
                 else
                 {
+                    _logger.LogInformation("---------------- ERROR AL TRATAR DE CREAR REGISTRO EN CREATE POST PATIENT CONTROLLER ---------");
                     ViewBag.ErrorMessage = "ERROR: NO SE REGISTRO";
                     return View(patient);
                 }
@@ -63,6 +73,7 @@ namespace Consultorio_Medico.MVC.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError("------------ ERROR : " + ex.Message + " -----------------");
                 ViewBag.ErrorMessage = ex.Message;
                 return View();
             }
@@ -71,8 +82,9 @@ namespace Consultorio_Medico.MVC.Controllers
         // GET: PatientController/Edit/5
         public async Task<ActionResult> Edit(int Id)
         {
+            _logger.LogInformation("-------------- INICIO METODO EDIT GET PATIENT CONTROLLER --------------------");
             var Patient = await _patientBL.GetById(Id);
-
+            _logger.LogInformation("-------------- FIN METODO EDIT GET PATIENT CONTROLLER ----------------");
             return View(new patientAddDTO
             {
                 patientId = Patient.patientId,
@@ -90,17 +102,23 @@ namespace Consultorio_Medico.MVC.Controllers
         {
             try
             {
+                _logger.LogInformation("------------------ INICIO METODO EDIT POST PATIENT CONTROLLER ----------------------");
                 int result = await _patientBL.Update(pPatient);
                 if (result > 0)
+                {
+                    _logger.LogInformation("------------- REGISTRO EDITADO CORRECTAMENTE: EDIT POST PATIENT CONTROLLER -----------------");
                     return RedirectToAction(nameof(Index));
+                }
                 else
                 {
+                    _logger.LogInformation("------------ ERROR AL TRATAR DE EDITAR EL REGISTRO: EDIT POST PATIENT CONTROLLER");
                     ViewBag.ErrorMessage = "ERROR: NO SE MODIFICO";
                     return View(pPatient);
                 }
             }
             catch (Exception ex)
             {
+                _logger.LogError("------------- ERROR : " + ex.Message + " -------------------------");
                 ViewBag.ErrorMessage = ex.Message;
                 return View();
             }
@@ -118,18 +136,24 @@ namespace Consultorio_Medico.MVC.Controllers
         public async Task<ActionResult> Delete(int Id, patientSearchOutputDTO pPatient)
         {
             try
-            {               
+            {
+                _logger.LogInformation("------------ INICIO METODO DELETE POST PATIENT CONTROLLER -------------------");
                 int result = await _patientBL.Delete(Id);
                 if (result != 0)
+                {
+                    _logger.LogInformation("------------ REGISTRO ELIMINADO CORRECTAMENTE: DELETE POST PATIENT CONTROLLER ----------------");
                     return RedirectToAction(nameof(Index));
+                }
                 else
                 {
+                    _logger.LogInformation("-------------- ERROR AL ELIMINAR REGISTRO: DELETE POST PATIENT CONTROLLER -----------------");
                     ViewBag.ErrorMessage = "Error no se pudo Eliminar";
                     return View(pPatient);
                 }
             }
-            catch
+            catch (Exception ex)
             {
+                _logger.LogError("-------------- ERROR : " + ex.Message + " -------------------");
                 return View(pPatient);
             }
         }
