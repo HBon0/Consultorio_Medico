0ad8c378ed6c728f93c52c1b3752c4a33c410647
Hector Bonilla <bonilla.osorio0@gmail.com>
2023-05-23 14:35:14 -0600

    Agregando Logica al controllador API Workplace

--- Consultorio_Medico.API/Controllers/WorkPlaceController.cs
+++ Consultorio_Medico.API/Controllers/WorkPlaceController.cs
@@ -1,6 +1,9 @@
 ﻿using Consultorio_Medico.BL.DTOs.WorkPlaceDTO;
+using Consultorio_Medico.BL.DTOs;
 using Consultorio_Medico.BL.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Consultorio_Medico.API.DTOs;
+using Consultorio_Medico.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,49 +15,153 @@ namespace Consultorio_Medico.API.Controllers
     {
         private readonly IWorkPlaceBL _workPlaceBL;
         private readonly ILogger<WorkPlaceController> _logger;
-        
+        DTOGenericResponse<List<WorkPlaceSearchOutPutDTO>> ListDTOresponse = new DTOGenericResponse<List<WorkPlaceSearchOutPutDTO>>();
+        DTOGenericResponse<WorkPlaceSearchOutPutDTO> DTOGenResponse = new DTOGenericResponse<WorkPlaceSearchOutPutDTO>();
+
         public WorkPlaceController (IWorkPlaceBL workPlaceBL, ILogger<WorkPlaceController> logger)
         {
-
             _workPlaceBL = workPlaceBL;
             _logger = logger;
         }
 
         // GET: api/<WorkPlaceController>
         [HttpGet]
-        public async Task<List<WorkPlaceSearchOutPutDTO>> Get()
+        public async Task<DTOGenericResponse<List<WorkPlaceSearchOutPutDTO>>> Get()
         {
-            _logger.LogInformation("------------------------------ INICIO DE METODO GET WORKPLACES --------------------------------");
-
+            _logger.LogInformation("------------- INICIO DE METODO GET WORKPLACES ----------------");
+            
             WokplaceSearchInputDTO wokplace = new WokplaceSearchInputDTO();
             var Workplaces = await _workPlaceBL.Search(wokplace);
 
-            return Workplaces;
+            var DTOGenResponse = ListDTOresponse.GetGenericResponse(true, "Obtencion de todos los registros", Workplaces);
+            _logger.LogInformation("---- FIN METODO GET WORKPLACES ----");
+            return DTOGenResponse;
         }
 
         // GET api/<WorkPlaceController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{Id}")]
+        public async Task<DTOGenericResponse<WorkPlaceSearchOutPutDTO>> GetById(int Id)
         {
-            return "value";
+            _logger.LogInformation("---- INICIO METODO GET BY ID WORKPLACES ----");
+            var workplace = await _workPlaceBL.GetById(Id);
+
+            var pDTOGenResponse = DTOGenResponse.GetGenericResponse(true, "Obteniendo Workplace", workplace);
+            _logger.LogInformation("---- FIN METODO GET BY ID WORKPLACES ----");
+            return pDTOGenResponse;
         }
 
         // POST api/<WorkPlaceController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<DTOGenericResponse<WorkPlaceSearchOutPutDTO>> Post(WorkPlaceInputDTO pWorkplace)
         {
+            try
+            {
+                _logger.LogInformation("---- INICIO METODO POST WORKPLACES ----");
+
+                var workplace = await _workPlaceBL.Create(pWorkplace);
+                if (workplace > 0)
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(true, "Creacion correcta", new WorkPlaceSearchOutPutDTO()
+                    {
+                        WorkPlacesId = pWorkplace.WorkPlacesId,
+                        WorkPlaces = pWorkplace.WorkPlaces
+                    });
+                    _logger.LogInformation("---- FIN METODO POST WORKPLACES ----");
+                    return pDTOGenResponse;
+                }
+                else
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(false, "Error al crear", new WorkPlaceSearchOutPutDTO()
+                    {
+                        WorkPlacesId = pWorkplace.WorkPlacesId,
+                        WorkPlaces = pWorkplace.WorkPlaces
+                    });
+                    _logger.LogWarning("---- ERROR EN METODO POST WORKPLACES ----");
+                    return pDTOGenResponse;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("--- ERROR : " + ex.Message + " ----");
+                var DTOGenRes = DTOGenResponse.GetGenericResponse(false, "Error : " + ex.Message, new WorkPlaceSearchOutPutDTO()
+                {
+                    WorkPlacesId = pWorkplace.WorkPlacesId,
+                    WorkPlaces = pWorkplace.WorkPlaces
+                });
+                return DTOGenRes;
+            }
         }
 
         // PUT api/<WorkPlaceController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("{Id}")]
+        public async Task<DTOGenericResponse<WorkPlaceSearchOutPutDTO>> Put(int Id, WorkPlaceInputDTO pWorkplace)
         {
+            try
+            {
+                _logger.LogInformation("---- INICIO METODO PUT WORKPLACES ----");
+
+                var workplace = await _workPlaceBL.Create(pWorkplace);
+                if (workplace > 0)
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(true, "Modificacion correcta", new WorkPlaceSearchOutPutDTO()
+                    {
+                        WorkPlacesId = pWorkplace.WorkPlacesId,
+                        WorkPlaces = pWorkplace.WorkPlaces
+                    });
+                    _logger.LogInformation("---- FIN METODO PUT WORKPLACES ----");
+                    return pDTOGenResponse;
+                }
+                else
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(false, "Error al crear", new WorkPlaceSearchOutPutDTO()
+                    {
+                        WorkPlacesId = pWorkplace.WorkPlacesId,
+                        WorkPlaces = pWorkplace.WorkPlaces
+                    });
+                    _logger.LogWarning("---- ERROR EN METODO PUT WORKPLACES ----");
+                    return pDTOGenResponse;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("--- ERROR : " + ex.Message + " ----");
+                var DTOGenRes = DTOGenResponse.GetGenericResponse(false, "Error : " + ex.Message, new WorkPlaceSearchOutPutDTO()
+                {
+                    WorkPlacesId = pWorkplace.WorkPlacesId,
+                    WorkPlaces = pWorkplace.WorkPlaces
+                });
+                return DTOGenRes;
+            }
         }
 
         // DELETE api/<WorkPlaceController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("{Id}")]
+        public async Task<DTOGenericResponse<WorkPlaceSearchOutPutDTO>> Delete(int Id)
         {
+            try
+            {
+                _logger.LogInformation("---- INICIO METODO DELETE WORKPLACES ----");
+
+                var workplace = await _workPlaceBL.Delete(Id);
+                if (workplace > 0)
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(true, "Eliminacion correcta", null);
+                    _logger.LogInformation("---- FIN METODO DELETE WORKPLACES ----");
+                    return pDTOGenResponse;
+                }
+                else
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(false, "Error al crear", null);
+                    _logger.LogWarning("---- ERROR EN METODO DELETE WORKPLACES ----");
+                    return pDTOGenResponse;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("--- ERROR : " + ex.Message + " ----");
+                var DTOGenRes = DTOGenResponse.GetGenericResponse(false, "Error : " + ex.Message, null);
+                return DTOGenRes;
+            }
         }
     }
 }
---
+++ Consultorio_Medico.API/DTOGenericResponse/DTOGenericResponse.cs
@@ -0,0 +1,20 @@
+﻿using Consultorio_Medico.API;
+
+namespace Consultorio_Medico.API.DTOs
+{
+    public class DTOGenericResponse <T> where T : class
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+
+        public DTOGenericResponse<T> GetGenericResponse(bool pSucces, string pMessage, T pData)
+        {
+            DTOGenericResponse<T> response = new DTOGenericResponse<T>();
+            response.Message = pMessage;
+            response.Data = pData;
+            response.Success = pSucces;
+            return response;
+        }
+    }
+}
