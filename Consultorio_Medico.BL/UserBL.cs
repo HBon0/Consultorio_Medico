e1459c410c8a3ec223a569528adf34b175ac2e9e
Hector Bonilla <bonilla.osorio0@gmail.com>
2023-05-30 12:02:20 -0600

    Agregando Logica de API a UserBL

--- Consultorio_Medico.BL/UserBL.cs
+++ Consultorio_Medico.BL/UserBL.cs
@@ -1,13 +1,17 @@
 ﻿using Consultorio_Medico.BL.DTOs.DoctorSpecialtiesDTO;
+using Consultorio_Medico.BL.DTOs.DTOGenericResponse;
 using Consultorio_Medico.BL.DTOs.DTOs;
 using Consultorio_Medico.BL.DTOs.userDTO;
+using Consultorio_Medico.BL.DTOs.UserSchedule;
 using Consultorio_Medico.BL.Interfaces;
 using Consultorio_Medico.Entities;
 using Consultorio_Medico.Entities.Interfaces;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -19,123 +23,114 @@ namespace Consultorio_Medico.BL
         private readonly IDoctorSpecialtiesBL _doctorSpecialties;
         private readonly ISecurityDAL _securityDAL;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration _configuration;
+        HttpClient client = new HttpClient();
 
-        public UserBL(IUserDAL userDAL, IUnitOfWork unitOfWork, ISecurityDAL security, IDoctorSpecialtiesBL doctorSpecialties)
+        public UserBL(IUserDAL userDAL, IUnitOfWork unitOfWork, ISecurityDAL security, IDoctorSpecialtiesBL doctorSpecialties, IConfiguration config)
         {
 
             _securityDAL = security;
             _userDAL = userDAL;
             _doctorSpecialties = doctorSpecialties;
-            _unitOfWork = unitOfWork;   
+            _unitOfWork = unitOfWork;
+            _configuration = config;
+        }
+        public string GetUrlAPI()
+        {
+            string ApiUrlBase = _configuration.GetValue<string>("ApiConnectionString");
+            ApiUrlBase += "User";
+            return ApiUrlBase;
         }
 
         public async Task<int> Create(UserAddDTO pUser)
         {
             try
             {
-                #region Crear objeto de Users
-                Users user = new Users()
+                string ApiUrl = GetUrlAPI();
+
+                string JsonUser = JsonSerializer.Serialize(pUser);
+                StringContent content = new StringContent(JsonUser, Encoding.UTF8, "application/json");
+
+                var HttpResponse = await client.PostAsync(ApiUrl, content);
+                if (HttpResponse.IsSuccessStatusCode)
                 {
-
-                    RolId = pUser.RolId,
-                    WorkplaceId = pUser.WorkplaceId,
-                    Name = pUser.Name,
-                    LastName = pUser.LastName,
-                    PhoneNumber = pUser.PhoneNumber,
-                    Dui = pUser.Dui,
-                    Email = pUser.Email,
-                    Login = pUser.Login,
-                    Password = pUser.Password,
-                    Status = pUser.Status,
-                    FechaRegistro = pUser.FechaRegistro,
-
-                };
-                #endregion
-
-                #region VALIDAR LOGIN Y ENCRIPTAR PASSWORD
-                bool ExisteLogin = _securityDAL.ValidateLogin(user);
-                if (ExisteLogin)
-                    throw new ArgumentException("El Login ya existe, Intente uno diferente");
-                string pass = user.Password;
-                user.Password = _securityDAL.EncriptarSHA256(pass); 
-                #endregion
-
-                _userDAL.Create(user);
-                var result = await _unitOfWork.SaveChangesAsync();
-
-                #region DoctorSpecialtie
-                //foreach (var item in pUser.DoctorSpecialtie)
-                //{
-                //    item.UserId = user.UserId;
-                //}
-                //var response = await _doctorSpecialties.CreateList(pUser.DoctorSpecialtie); 
-                #endregion
-
-                if (result > 0 /*&& response > 0*/)
-                    return result;
+                    var contentResponse = await HttpResponse.Content.ReadAsStringAsync();
+                    var User = JsonSerializer.Deserialize<DTOGenericResponse<userSearchOutputDTO>>(contentResponse, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    return 1;
+                }
                 return 0;
             }
             catch (Exception e)
             {
+
                 return 0;
             }
         }
 
         public async Task<int> Delete(int Id)
         {
-            Users UserEn = await _userDAL.GetById(Id);
-            if (UserEn.UserId == Id)
+            try
             {
-                _userDAL.Delete(UserEn);
-                return await _unitOfWork.SaveChangesAsync();
-            }
-            else
+                string ApiUrlBase = GetUrlAPI();
+                var HttpResponse = await client.DeleteAsync(ApiUrlBase + $"/{Id}");
+
+                if (HttpResponse.IsSuccessStatusCode)
+                {
+                    var content = await HttpResponse.Content.ReadAsStringAsync();
+                    var User = JsonSerializer.Deserialize<DTOGenericResponse<userSearchOutputDTO>>(content, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    return 1;
+                }
                 return 0;
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
 
         }
 
         public async Task<userSearchOutputDTO> GetById(int Id)
         {
-            Users UserEn = await _userDAL.GetById(Id);
-            return new userSearchOutputDTO()
+            try
             {
-                UserId = UserEn.UserId,
-                RolId = UserEn.RolId,
-                WorkplaceId = UserEn.WorkplaceId,
-                Name= UserEn.Name,
-                LastName= UserEn.LastName,
-                PhoneNumber = UserEn.PhoneNumber,
-                Dui = UserEn.Dui,
-                Email = UserEn.Email,
-                Login = UserEn.Login,
-                Status = UserEn.Status,
-                FechaRegistro = UserEn.FechaRegistro    
+                string ApiUrlBase = GetUrlAPI();
+                var HttpResponse = await client.GetAsync(ApiUrlBase + $"/{Id}");
 
-            };
+                if (HttpResponse.IsSuccessStatusCode)
+                {
+                    var content = await HttpResponse.Content.ReadAsStringAsync();
+                    var User = JsonSerializer.Deserialize<DTOGenericResponse<userSearchOutputDTO>>(content, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    return User.Data;
+                }
+                return new userSearchOutputDTO();
+            }
+            catch (Exception e)
+            {
+                return new userSearchOutputDTO();
+            }
 
         }
 
         public async Task<List<userSearchOutputDTO>> Search(userSearchInputDTO pUser)
         {
-            List<Users> users = await _userDAL.Search(new Users { Name = pUser.NameLike, PhoneNumber = pUser.PhonNumberLike });
-            List<userSearchOutputDTO> list = new List<userSearchOutputDTO>();
-            users.ForEach(s => list.Add(new userSearchOutputDTO
+            try
             {
-                UserId = s.UserId,
-                RolId = s.RolId,
-                WorkplaceId = s.WorkplaceId,
-                Name = s.Name,
-                LastName = s.LastName,
-                PhoneNumber = s.PhoneNumber,
-                Dui = s.Dui,
-                Email = s.Email,
-                Status = s.Status,
-                Login = s.Login,
-                RolName =s.Rol.Name,
-                WorkPlaceName=s.WorkPlace.WorkPlaces,
-            })) ;
-            return list;
+                string ApiUrlBase = GetUrlAPI();
+                var HttpResponse = await client.GetAsync(ApiUrlBase);
+
+                if (HttpResponse.IsSuccessStatusCode)
+                {
+                    var content = await HttpResponse.Content.ReadAsStringAsync();
+                    var User = JsonSerializer.Deserialize<DTOGenericResponse<List<userSearchOutputDTO>>>(content, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    return User.Data;
+                }
+                return new List<userSearchOutputDTO>();
             }
+            catch (Exception e)
+            {
+                return new List<userSearchOutputDTO>();
+            }
+        }
 
         public async Task<int> Update(userUpdateDTO pUser)
         {
