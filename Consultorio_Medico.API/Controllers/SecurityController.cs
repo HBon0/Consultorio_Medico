bc4e9f02f1c733f2aff7487a60eafc761933a015
Hector Bonilla <bonilla.osorio0@gmail.com>
2023-05-31 11:16:07 -0600

    Agregando SecurityController a API

---
+++ Consultorio_Medico.API/Controllers/SecurityController.cs
@@ -0,0 +1,59 @@
+﻿using Consultorio_Medico.API.DTOs;
+using Consultorio_Medico.BL;
+using Consultorio_Medico.BL.DTOs.SpecialtiesDTO;
+using Consultorio_Medico.BL.DTOs.userDTO;
+using Consultorio_Medico.BL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Consultorio_Medico.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        private readonly ISecurityBL _securityBL;
+        private readonly ILogger<SecurityController> _logger;
+
+        DTOGenericResponse<securityDTO> DTOGenResponse = new DTOGenericResponse<securityDTO>();
+
+        public SecurityController (ISecurityBL securityBL, ILogger<SecurityController> logger)
+        {
+            _securityBL = securityBL;
+            _logger = logger;
+        }
+
+        // POST api/<SecurityController>
+        [HttpPost]
+        public DTOGenericResponse<securityDTO> Post(string Login, string Password)
+        {
+            try
+            {
+                _logger.LogInformation("---- INICIO METODO POST LOGIN POST API CONTROLLER ----");
+
+                var security = _securityBL.Login(Login, Password);
+                if (security.userId > 0)
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(true, "Logeo correcto",security);
+                    _logger.LogInformation("---- FIN METODO POST LOGIN POST API CONTROLLER ----");
+                    return pDTOGenResponse;
+                }
+                else
+                {
+                    var pDTOGenResponse = DTOGenResponse.GetGenericResponse(false, "Error al Logeo, credenciales incorrectas", null);
+                    _logger.LogWarning("---- ERROR EN METODO POST SPECIALTIE API CONTROLLER ----");
+                    return pDTOGenResponse;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("--- ERROR : " + ex.Message + " ----");
+                var DTOGenRes = DTOGenResponse.GetGenericResponse(false, "Error : " + ex.Message, null);
+                return DTOGenRes;
+            }
+        }
+
+        
+    }
+}
